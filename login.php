af4e8683727ef2ab2118b9e7f596e83a0dfb0e35
Gandhi Balingkit <balingkitgandhi@gmail.com>
2022-09-29 11:03:57 +0800

    Update login.php

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
 define('DB_HOST','localhost');
 define('DB_USERNAME','root');
 define('DB_PASSWORD','');
-define('DB_NAME', 'tccac');
+define('DB_NAME', 'testdb');
 
 //Connecting to the database
 $conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
